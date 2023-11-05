6f2625a4220cc9d28178f2237e7c45b7f3de02f4
Nisha Sharma <124479532+hhggfdsds@users.noreply.github.com>
2023-11-04 19:37:09 -0700

    My first project

--- devopsProject/src/Newproject/GitpushProject.java
+++ devopsProject/src/Newproject/GitpushProject.java
@@ -5,6 +5,11 @@ public static void main(String[] args) {
 		
 		System.out.println("Welcome to Code Lab Technology");
 		System.out.println("My first project");
+		System.out.println("I am the perfect");
+		System.out.println("I am Devops engineer");
+		System.out.println("I did it");
+		System.out.println("happy horrary");
+		
 		
 
 }
